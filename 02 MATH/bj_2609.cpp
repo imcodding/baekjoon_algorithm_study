3d68047c24a2d28550b85dee67b6cbbd19fdae33
imcodding <suy0226@naver.com>
2021-05-11 15:29:43 +0900

    최대공약수와 최소공배수

---
+++ 02 MATH/bj_2609.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int gcd(int x, int y) { // 최대공약수
+	if (y == 0) return x;
+	else gcd(y, x % y);
+}
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	int g = gcd(a, b);
+	int l = a * b / g; // 최소공배수
+	cout << g << '\n' << l;
+	return 0;
+}
