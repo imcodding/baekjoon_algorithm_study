644138672530c6237feb3ba15a6c2952c52b7300
imcodding <suy0226@naver.com>
2021-05-11 14:32:27 +0900

    약수의 합

--- 02 MATH/bj_17427.cpp
+++ 02 MATH/bj_17427.cpp
@@ -5,7 +5,10 @@ int main()
 {
 	int n;
 	cin >> n;
-	long long sum = 0;
+	long long sum = 0; // int 불가능. 문제크기 확인
+	// *Point : A는 약수의 배수이다.
+	// 8의 약수 => 1,2,4,8.
+	// 8은 1,2,4,8의 배수이다.
 	for (int i = 1; i <= n; i++) {
 		sum += (n / i) * i;
 	}
