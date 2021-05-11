ee2eae50157eb976c6b52c710fb8d2a896aa44b2
imcodding <suy0226@naver.com>
2021-05-11 15:59:34 +0900

    소수 찾기

---
+++ 02 MATH/bj_1978.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+bool isPrime(int n) {
+	if (n < 2) return false;
+	// 루트 n까지 구하면 됨. => i*i < n
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) return false;
+	}
+	return true;
+}
+int main()
+{
+	int n;
+	cin >> n;
+	int count = 0;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		if (isPrime(num)) {
+			count += 1;
+		}
+	}
+	cout << count << '\n';
+	return 0;
+}
